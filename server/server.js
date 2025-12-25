b682c9f0c73f04c210538fda3abea7d4de3fea00
owennam <netter110715@gmail.com>
2025-12-25 20:34:39 +0900

    fix(server): Add PATCH method to allowed CORS methods to fix admin approval error

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
